453dd33afb74d43840bb3c110d5a20b328d938d1
SungoDex <raphamotta2015@gmail.com>
2022-11-23 12:37:34 -0300

    first commit

---
+++ assets/js/heightAdjust.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  $(".homepage").css({
+    marginTop: $(".nav-bar").height() + "px",
+  });
+});
---
+++ assets/js/transparentMenu.js
@@ -0,0 +1,13 @@
+/* $(document).ready(function () {
+  $(window).bind("scroll", function () {
+    var gap = 50;
+    if ($(window).scrollTop() > gap) {
+      $(".nav-bar").addClass("active");
+      $("li").addClass("color-hover");
+    } else {
+      $(".nav-bar").removeClass("active");
+      $("li").removeClass("color-hover");
+    }
+  });
+});
+*/
